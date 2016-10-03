ede0862b26cd929de65e8de6ff0ee1377aed3874
Arun Wagle <arunwagle@Aruns-MBP.fios-router.home>
2016-10-03 13:02:13 -0400

    IBM Projects

---
+++ ApacheSpark/ibm-spark-scala/docs/Centos-script-do.sh
@@ -0,0 +1,14 @@
+declare -a nodes=(
+"h1.aw.com"
+"h2.aw.com"
+"h3.aw.com"
+"h4.aw.com"
+"h5.aw.com"
+)
+
+
+for i in "${nodes[@]}"
+do
+    ssh "$i" "$1"
+done
+
---
+++ ApacheSpark/ibm-spark-scala/ibm-demos/src/main/scala/com/ibm/demos/finance/cfpb/cc/driver/ComplaintData.scala
@@ -0,0 +1,3 @@
+package com.ibm.demos.finance.cfpb.cc.driver
+
+case class ComplaintData(day:Int, product: String, issue: String, company: String, state: String)
